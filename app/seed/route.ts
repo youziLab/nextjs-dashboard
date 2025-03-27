d81ad99a7d31e8aaf6f5d3220b7940f31f71c166
youzi <youzibycc@gmail.com>
2025-03-28 02:40:55 +0800

    增加了bcryptjs

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,5 +1,5 @@
-import bcrypt from 'bcrypt';
-// import bcrypt from "bcryptjs";
+// import bcrypt from 'bcrypt';
+import bcrypt from "bcryptjs";
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
