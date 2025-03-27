e938f2dab45168d7e809b9cd7e47646e7bae0c13
youzi <youzibycc@gmail.com>
2025-03-28 02:17:51 +0800

    第六章报错，修改import bcrypt from "bcryptjs";

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,4 +1,5 @@
-import bcrypt from 'bcrypt';
+// import bcrypt from 'bcrypt';
+import bcrypt from "bcryptjs";
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
